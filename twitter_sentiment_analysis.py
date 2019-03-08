24147836dec33045372c6d235d59cc777b1765a4
shimona_elora <umysharma@gmail.com>
2019-03-08 21:07:51 +0530

    Tweet count increased

--- twitter_sentiment_analysis.py
+++ twitter_sentiment_analysis.py
@@ -2,6 +2,7 @@ import tweepy
 import pandas as pd
 import sys
 import csv
+import pickle
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import matplotlib.pyplot as plt
 from collections import Counter
@@ -15,7 +16,8 @@ import secret
 # authenicating with authentication variables
 auth = tweepy.OAuthHandler(secret.consumer_key, secret.consumer_secret)
 auth.set_access_token(secret.access_token, secret.access_token_secret)
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True,
+				   wait_on_rate_limit_notify=True)
 # Error handling
 if (not api):
     print("Problem Connecting to API")
@@ -28,22 +30,91 @@ location = input("What is the location of party? \n")
 # Getting Geo ID for Places
 places = api.geo_search(query=location)
 # place id
-print(places[0].id)
+print(places[0])
 
-# search tweets with some keywords
-results = api.search(hash_tag,
-                     count=number,
-                     tweet_mode='extended',
-                     lang='en',
-                     place=places[0].id
-                     )
+tweetsPerQry = 100  # this is the max the API permits
 
-tweets = results
-data = pd.DataFrame(data=[tweet.full_text for tweet in tweets], columns=['Tweets'])
-data.to_csv('output.csv')
+
+# If results from a specific ID onwards are reqd, set since_id to that ID.
+# else default to no lower limit, go as far back as API allows
+sinceId = None
+
+# If results only below a specific ID are, set max_id to that ID.
+# else default to no upper limit, start from the most recent tweet matching the search query.
+max_id = -1
+data = None
+
+dataset = []
+outputFile = 'test.data'
+fw = open(outputFile, 'wb')
+
+tweetCount = 0
+print("Downloading max {0} tweets".format(number))
+while tweetCount < number:
+	try:
+		if (max_id <= 0):
+			if (not sinceId):
+				new_tweets = api.search(q=hash_tag, count=tweetsPerQry, tweet_mode='extended', lang='en')
+				tweetCount += len(new_tweets)
+				print("Downloaded {0} tweets".format(tweetCount))
+			else:
+				new_tweets = api.search(q=hash_tag, count=tweetsPerQry,
+					since_id=sinceId, tweet_mode='extended', lang='en')
+				print("here 2")
+		else:
+			if (not sinceId):
+				new_tweets = api.search(q=hash_tag, count=tweetsPerQry,
+					max_id=str(max_id - 1), tweet_mode='extended', lang='en')
+				print("here 3")
+			else:
+				new_tweets = api.search(q=hash_tag, count=tweetsPerQry,
+					max_id=str(max_id - 1),
+					since_id=sinceId, tweet_mode='extended', lang='en')
+				print("here 4")
+			if not new_tweets:
+				print("No more tweets found")
+				break
+
+		#print(new_tweets)
+
+		tweets = new_tweets
+		for tweet in tweets:
+			dataset.append(tweet.full_text)
+		#data += pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
+		tweetCount += len(new_tweets)
+		print("Downloaded {0} tweets".format(tweetCount))
+		max_id = new_tweets[-1].id
+	except tweepy.TweepError as e:
+		print(str(e))
+		break
+
+
+pickle.dump(dataset, fw)
+fw.close()
+
+inputFile = 'test.data'
+fd = open(inputFile, 'rb')
+dataset = pickle.load(fd)
+print(dataset)
+
+#search tweets with some keywords
+# results = api.search(hash_tag,
+#                      count=number,
+#                      since="2019-03-04",
+# 					 until="2019-03-05",
+#                      tweet_mode='extended',
+#                      lang='en',
+#                      place=places[0].id
+#                      )
+
+# print (results)
+
+# tweets = results
+# data = pd.DataFrame(data=[tweet.full_text for tweet in tweets], columns=['Tweets'])
+#data.to_csv('output.csv')
 
 # print the first 10 data
-print(data.head(10))
+#print(data)
 
 import nltk
 nltk.download('vader_lexicon')
@@ -54,8 +125,8 @@ sid = SentimentIntensityAnalyzer()
 l = []
 counter = Counter()
 
-for index, row in data.iterrows():
-    ss = sid.polarity_scores(row["Tweets"])
+for data in dataset:
+    ss = sid.polarity_scores(data)
     l.append(ss)
     k = ss['compound']
     if k >= 0.05:
@@ -64,11 +135,6 @@ for index, row in data.iterrows():
         counter['negative'] += 1
     else:
         counter['neutral'] += 1
-
-se = pd.Series(l)
-data['polarity'] = se.values
-# to print in tabular form
-print(data.head(100))
 
 positive = counter['positive']
 negative = counter['negative']
